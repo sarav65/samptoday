6b780371dc9d5b112f3b163660d8105b25a9a50e
jeeva <jeeva@DESKTOP-H44QACB>
2018-09-27 18:53:26 +0530

    newly commit

--- sampletoday/src/sampletodaypack/npgrm.java
+++ sampletoday/src/sampletodaypack/npgrm.java
@@ -6,7 +6,8 @@ public class npgrm {
 		// TODO Auto-generated method stub
 System.out.println("welcome");
 System.out.println("new program");	
-}System.out.println("demo program");
+System.out.println("demo program");
+	}
 
 
 }
