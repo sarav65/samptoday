1b19d928bce00842a1b1798f284cfa2640b79e8e
jeeva <jeeva@DESKTOP-H44QACB>
2018-09-27 19:03:28 +0530

    commit check

---
+++ newtria/src/newtria/newdemo.java
@@ -0,0 +1,10 @@
+package newtria;
+
+public class newdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to demo program");
+	}
+
+}
