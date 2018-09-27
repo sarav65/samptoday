45c780c112a369135b8538a9576a8001adcefd09
jeeva <jeeva@DESKTOP-H44QACB>
2018-09-27 15:02:11 +0530

    newly added a sampdemo and commited

---
+++ sampletoday/src/sampletodaypack/sampletoda.java
@@ -0,0 +1,10 @@
+package sampletodaypack;
+
+public class sampletoda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("sample program today");
+	}
+
+}
